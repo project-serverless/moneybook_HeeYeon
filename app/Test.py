81b7fa4915830ea152ba7ac25cb9359f93b92c43
Happy-Lotus <khyeon2020@naver.com>
2023-07-19 17:11:43 +0900

    invoke

--- app/Test.py
+++ app/Test.py
@@ -1,32 +1,18 @@
-import dotenv
+import dotenv 
 import logging
 import boto3
 from botocore.exceptions import ClientError
 import os
-
+import json
 
 dotenv.load_dotenv(".env",override=True)
 
-def upload_file(file_name, bucket, object_name=None):
-    """Upload a file to an S3 bucket
-
-    :param file_name: File to upload
-    :param bucket: Bucket to upload to
-    :param object_name: S3 object name. If not specified then file_name is used
-    :return: True if file was uploaded, else False
-    """
-
-    # If S3 object_name was not specified, use file_name
-    if object_name is None:
-        object_name = os.path.basename(file_name)
-
-    # Upload the file
-    s3_client = boto3.client('s3')
-    try:
-        response = s3_client.upload_file(file_name, bucket, object_name)
-    except ClientError as e:
-        logging.error(e)
-        return False
+def create_new_file(file_name):
+    lambda_client = boto3.client('lambda')
+    lambda_client.invoke(FunctionName='createCSVFile',
+                         InvocationType='RequestResponse',
+                         Payload=json.dumps({
+                             "file_name":file_name
+                         })
+                         )
     return True
-
-upload_file('tmp.json', 'your-bucket', 'tmp.json')
