a7847f905f61985fb0e076ff7d8a3b24ce5a98a9
Happy-Lotus <khyeon2020@naver.com>
2023-07-26 14:59:06 +0900

    lambda error

--- cdk/lib/hello-cdk-stack.ts
+++ cdk/lib/hello-cdk-stack.ts
@@ -1,16 +1,23 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { HelloCdkS3Stack } from "./stack/s3-stack";
+import { MoneybookLambdaStack } from "./stack/lambda-stack";
+import { Account } from "./config/accounts";
+import { SYSTEM_NAME } from "./config/common";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+export interface HelloCdkStackProps extends cdk.StackProps {
+  context: Account;
+  s3Stack?: HelloCdkS3Stack;
+}
+
 export class HelloCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: HelloCdkStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const s3Stack = new HelloCdkS3Stack(this, `${SYSTEM_NAME}-s3Stack`, props);
+    props.s3Stack = s3Stack;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new MoneybookLambdaStack(this, `${SYSTEM_NAME}-lambdaStack`, props);
   }
 }
