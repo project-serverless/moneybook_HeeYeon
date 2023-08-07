141f1b2fdc5e26cc01a523c15e5521e0df23d97e
HeeYeon-Kim <62700196+Happy-Lotus@users.noreply.github.com>
2023-08-07 23:29:47 +0900

    Update lambda-stack.ts

--- cdk/lib/stack/lambda-stack.ts
+++ cdk/lib/stack/lambda-stack.ts
@@ -3,7 +3,7 @@ import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { getAccountUniqueName } from "../config/accounts";
 import { HelloCdkStackProps } from "../hello-cdk-stack";
-import { SYSTEM_NAME } from "../config/common";
+import { SYSTEM_NAME } from "../config/commons";
 import { PythonFunction } from "@aws-cdk/aws-lambda-python-alpha";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import * as path from "path";
@@ -16,8 +16,16 @@ import {
   PolicyStatement,
   Effect,
 } from "aws-cdk-lib/aws-iam";
+import {
+  RestApi,
+  EndpointType,
+  MethodLoggingLevel,
+  LogGroupLogDestination,
+  LambdaIntegration,
+} from "aws-cdk-lib/aws-apigateway";
+import { LogGroup } from "aws-cdk-lib/aws-logs";
 
-export class MoneybookLambdaStack extends cdk.Stack {
+export class MoneybookApiStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: HelloCdkStackProps) {
     super(scope, id, props);
 
@@ -31,20 +39,106 @@ export class MoneybookLambdaStack extends cdk.Stack {
           "service-role/AWSLambdaBasicExecutionRole"
         ),
         ManagedPolicy.fromAwsManagedPolicyName("AmazonS3FullAccess"),
+        ManagedPolicy.fromAwsManagedPolicyName("AmazonDynamoDBFullAccess"),
       ],
     });
 
     // index.py -> lambda_handler
     //
-    new PythonFunction(this, `${SYSTEM_NAME}-create-file`, {
-      functionName: `${getAccountUniqueName(props.context)}-create-file`,
-      entry: path.join(__dirname, "../../../app/backend/create-file"),
-      index: "create_file.py",
-      runtime: Runtime.PYTHON_3_10,
-      role: lambdaRole,
-      environment: {
-        BUCKET_NAME: props.s3Stack!.bucket.bucketName,
+    const createFileFunction = new PythonFunction(
+      this,
+      `${SYSTEM_NAME}-create-file`,
+      {
+        functionName: `${getAccountUniqueName(props.context)}-create-file`,
+        entry: path.join(__dirname, "../../../app/backend/create-file"),
+        index: "create_file.py",
+        handler: "lambda_handler",
+        runtime: Runtime.PYTHON_3_10,
+        role: lambdaRole,
+        environment: {
+          BUCKET_NAME: props.s3Stack!.bucket.bucketName,
+        },
+      }
+    );
+
+    const addMoneybookFunction = new PythonFunction(
+      this,
+      `${SYSTEM_NAME}-add-moneybook-item`,
+      {
+        functionName: `${getAccountUniqueName(
+          props.context
+        )}-add-moneybook-item`,
+        entry: path.join(__dirname, "../../../app/backend/add-moneybook-item"),
+        runtime: Runtime.PYTHON_3_10,
+        role: lambdaRole,
+        index: "index.py", // file name
+        handler: "lambda_handler", // function name
+        environment: {
+          TABLE_NAME: props.dynamoStack!.moneybookTable.tableName,
+        },
+      }
+    );
+
+    const api = new RestApi(this, `${SYSTEM_NAME}-api`, {
+      restApiName: `${getAccountUniqueName(props.context)}-moneybook-api`,
+      description: "Moneybook Application API",
+      deployOptions: {
+        stageName: "dev",
+        metricsEnabled: true,
+        loggingLevel: MethodLoggingLevel.INFO,
+        accessLogDestination: new LogGroupLogDestination(
+          new LogGroup(
+            this,
+            `${getAccountUniqueName(props.context)}-api-log-group`,
+            {
+              logGroupName: `/API-Gateway/${getAccountUniqueName(
+                props.context
+              )}-moneybook-api`,
+              removalPolicy: props.terminationProtection
+                ? cdk.RemovalPolicy.RETAIN
+                : cdk.RemovalPolicy.DESTROY,
+            }
+          )
+        ),
       },
+      endpointTypes: [EndpointType.REGIONAL],
+      retainDeployments: props.terminationProtection,
+      cloudWatchRole: true,
     });
+
+    const apiKey = api.addApiKey(`${SYSTEM_NAME}-ApiKey`, {
+      apiKeyName: `${getAccountUniqueName(props.context)}-ApiKey`,
+      description: "Moneybook API Key",
+    });
+
+    const usagePlan = api.addUsagePlan(`${SYSTEM_NAME}-UsagePlan`, {
+      name: `${getAccountUniqueName(props.context)}-UsagePlan`,
+      description: "Moneybook Usage Plan",
+      apiStages: [
+        {
+          api: api,
+          stage: api.deploymentStage,
+        },
+      ],
+    });
+    usagePlan.addApiKey(apiKey);
+
+    const methodOptions = {
+      apiKeyRequired: true,
+    };
+
+    const fileResource = api.root.addResource("file");
+    fileResource.addMethod(
+      "POST",
+      new LambdaIntegration(createFileFunction),
+      methodOptions
+    );
+
+    const moneybookResource = api.root.addResource("moneybook");
+    moneybookResource.addMethod(
+      "POST",
+      new LambdaIntegration(addMoneybookFunction),
+      methodOptions
+    );
   }
 }
