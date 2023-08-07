718ca716fb639714726db1d23c7fbf0643e3e872
Happy-Lotus <khyeon2020@naver.com>
2023-08-08 00:19:35 +0900

    dynamodb&apigateway

--- app/backend/add-moneybook-item/index.py
+++ app/backend/add-moneybook-item/index.py
@@ -1,1 +1,29 @@
+import json
+import boto3
+import os
+import time
 
+def lambda_handler(event, context):
+
+    user_id = event['body']['user_id']
+    cost = event['body']['cost']
+    name = event['body']['name']
+    timestamp = int(time.time())
+
+    table_name = os.getenv("TABLE_NAME")
+
+    dynamodb = boto3.resource('dynamodb')
+
+    table = dynamodb.Table(table_name)
+
+    table.put_item(Item={
+        'user-id': user_id,
+        'time': timestamp,
+        'cost': cost,
+        'name': name,
+    })
+
+    return {
+        'statusCode': 200,
+        'body': json.dumps(f'{name} saved!')
+    }
