bc75cbd2905e8530db9bb144b85750cdabc2263d
HeeYeon-Kim <62700196+Happy-Lotus@users.noreply.github.com>
2023-08-07 23:29:31 +0900

    Update hello-cdk-stack.ts

--- cdk/lib/hello-cdk-stack.ts
+++ cdk/lib/hello-cdk-stack.ts
@@ -1,6 +1,8 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { HelloCdkS3Stack } from "./stack/s3-stack";
+import { MoneybookApiStack } from "./stack/lambda-stack";
+import { HelloCdkDynamoStack } from "./stack/dynamo-stack";
 import { Account } from "./config/accounts";
 import { SYSTEM_NAME } from "./config/commons";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -8,6 +10,7 @@ import { SYSTEM_NAME } from "./config/commons";
 export interface HelloCdkStackProps extends cdk.StackProps {
   context: Account;
   s3Stack?: HelloCdkS3Stack;
+  dynamoStack?: HelloCdkDynamoStack;
 }
 
 export class HelloCdkStack extends cdk.Stack {
@@ -17,11 +20,13 @@ export class HelloCdkStack extends cdk.Stack {
     const s3Stack = new HelloCdkS3Stack(this, `${SYSTEM_NAME}-s3Stack`, props);
     props.s3Stack = s3Stack;
 
-    // The code that defines your stack goes here
+    const dynamoStack = new HelloCdkDynamoStack(
+      this,
+      `${SYSTEM_NAME}-dynamoStack`,
+      props
+    );
+    props.dynamoStack = dynamoStack;
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    new MoneybookApiStack(this, `${SYSTEM_NAME}-apiStack`, props);
   }
 }
